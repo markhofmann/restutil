55eb4320a30110360088cf53f369a71ff2bd14cf
Sina Taghva <s_taghva@live.com>
2016-09-02 20:46:35 +0430

    fixed headers not being sent

--- ios/src/main/java/com/rockstar/restutil/ios/IOSRestRequest.java
+++ ios/src/main/java/com/rockstar/restutil/ios/IOSRestRequest.java
@@ -22,37 +22,41 @@ public class IOSRestRequest extends BaseRestRequest {
         super(requestMethod, urll, restUtil);
     }
 
-    public void dummy() {
-      
-    }
+    public native void makeRequest() /*-[
+
+        [request setUrl:[self getUrl]];
+        [request setHeaders:headerDictionary];
+        [request setParameters:parameterDictionary];
+    ]-*/;
+
     /**
      * Native implementation of the execute method.
      * See {@link com.rockstar.restutil.common.RestRequest#execute(RestCallback)} for details.
      */
     @Override
     public native <T, E> void execute(RestCallback<T, E> callback) /*-[
-            NSDictionary* headers = @{@"accept": @"application/json"};
-            //NSDictionary* parameters = @{@"origin": @"35.776,51.464", @"destination": @"35.782776,51.433414"};
-            
-            // UNIHTTPJsonResponse *response = [[UNIRest get:^(UNISimpleRequest *request) {
-            //     [request setUrl:@"https://maps.googleapis.com/maps/api/directions/json"];
-            //     [request setHeaders:headers];
-            //     [request setParameters:parameters];
-            // }] asJson];
+      // NSDictionary* headers = @{@"accept": @"application/json"};
+      //NSDictionary* parameters = @{@"origin": @"35.776,51.464", @"destination": @"35.782776,51.433414"};
+      
+      // UNIHTTPJsonResponse *response = [[UNIRest get:^(UNISimpleRequest *request) {
+      //     [request setUrl:@"https://maps.googleapis.com/maps/api/directions/json"];
+      //     [request setHeaders:headers];
+      //     [request setParameters:parameters];
+      // }] asJson];
 
-            //Translate headers to NSDictionary
-            // JavaUtilHashMap *headerMap = [[JavaUtilHashMap alloc] initWithJavaUtilMap:headers_];
-            // NSMutableDictionary *headerDictionary = [[NSMutableDictionary alloc] init];
-            // id <JavaUtilIterator> iterator = [headerMap newKeyIterator];
-            // while ([iterator hasNext]) {
-            //     id object = [iterator next];
-            //     [headerDictionary setObject:[headerMap getWithId:object] forKey:object];
-            // }
+      // Translate headers to NSDictionary
+      JavaUtilHashMap *headerMap = [[JavaUtilHashMap alloc] initWithJavaUtilMap:headers_];
+      NSMutableDictionary *headerDictionary = [[NSMutableDictionary alloc] init];
+      id <JavaUtilIterator> iterator = [headerMap newKeyIterator];
+      while ([iterator hasNext]) {
+          id object = [iterator next];
+          [headerDictionary setObject:[headerMap getWithId:object] forKey:object];
+      }
 
-            //Translate parameters_ to NSDictionary
+      //Translate parameters_ to NSDictionary
       JavaUtilHashMap *parameterMap = [[JavaUtilHashMap alloc] initWithJavaUtilMap:parameters_];
       NSMutableDictionary *parameterDictionary = [[NSMutableDictionary alloc] init];
-      id <JavaUtilIterator> iterator = [parameterMap newKeyIterator];
+      iterator = [parameterMap newKeyIterator];
       while ([iterator hasNext]) {
           id object = [iterator next];
           [parameterDictionary setObject:[parameterMap getWithId:object] forKey:object];
@@ -63,12 +67,13 @@ public class IOSRestRequest extends BaseRestRequest {
           [parameterDictionary setObject:url forKey:@"fileUpload"];
       }
 
-      // NSLog(@"%@",parameterDictionary);
+      NSLog(@"params: %@",parameterDictionary);
+      NSLog(@"headers: %@",headerDictionary);
 
       if ([[[self getRequestMethod] getMethod] isEqualToString:@"GET"]) {
         [[UNIRest get:^(UNISimpleRequest *request) {
           [request setUrl:[self getUrl]];
-          [request setHeaders:headers];
+          [request setHeaders:headerDictionary];
           [request setParameters:parameterDictionary];
         }] asStringAsync:^(UNIHTTPStringResponse* response, NSError *error) {
           // This is the asyncronous callback block
@@ -101,7 +106,7 @@ public class IOSRestRequest extends BaseRestRequest {
       } else if ([[[self getRequestMethod] getMethod] isEqualToString:@"POST"]) {
         [[UNIRest post:^(UNISimpleRequest *request) {
           [request setUrl:[self getUrl]];
-          [request setHeaders:headers];
+          [request setHeaders:headerDictionary];
           [request setParameters:parameterDictionary];
         }] asStringAsync:^(UNIHTTPStringResponse* response, NSError *error) {
           // This is the asyncronous callback block
@@ -134,7 +139,7 @@ public class IOSRestRequest extends BaseRestRequest {
       } else if ([[[self getRequestMethod] getMethod] isEqualToString:@"PUT"]) {
         [[UNIRest put:^(UNISimpleRequest *request) {
           [request setUrl:[self getUrl]];
-          [request setHeaders:headers];
+          [request setHeaders:headerDictionary];
           [request setParameters:parameterDictionary];
         }] asStringAsync:^(UNIHTTPStringResponse* response, NSError *error) {
           // This is the asyncronous callback block
@@ -167,7 +172,7 @@ public class IOSRestRequest extends BaseRestRequest {
       } else if ([[[self getRequestMethod] getMethod] isEqualToString:@"DELETE"]) {
         [[UNIRest delete:^(UNISimpleRequest *request) {
           [request setUrl:[self getUrl]];
-          [request setHeaders:headers];
+          [request setHeaders:headerDictionary];
           [request setParameters:parameterDictionary];
         }] asStringAsync:^(UNIHTTPStringResponse* response, NSError *error) {
           // This is the asyncronous callback block

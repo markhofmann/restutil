a2d7ce54ef4e08c1ca6a718d8dd2fa28ace6215b
Mark Hofmann <mark@mark-hofmann.de>
2016-09-04 22:41:22 +0200

    changed to use Gson for Android

--- android/src/main/java/com/rockstar/restutil/android/AndroidRestUtil.java
+++ android/src/main/java/com/rockstar/restutil/android/AndroidRestUtil.java
@@ -5,25 +5,16 @@ import android.util.Log;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import com.rockstar.restutil.common.BaseRestUtil;
-import com.rockstar.restutil.common.RestCallback;
 import com.rockstar.restutil.common.RestRequest;
-import org.apache.commons.io.IOUtils;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
 
 /**
  * @author Mark Hofmann (mark@mark-hofmann.de)
  */
 public class AndroidRestUtil extends BaseRestUtil {
     private static final String TAG = AndroidRestUtil.class.getName();
-    private ObjectMapper objectMapper;
+    private Gson gson;
 
     private RequestQueue requestQueue;
 
@@ -44,37 +35,36 @@ public class AndroidRestUtil extends BaseRestUtil {
      */
     public AndroidRestUtil(Context context) {
         this.requestQueue = Volley.newRequestQueue(context);
-        this.objectMapper = new ObjectMapper();
-        this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        this.gson = new Gson();
     }
 
-    @Override
-    public <D, T, E> void post(String urlStr, String fileUri, D object, Map<String, Object> parameters,
-        Map<String, String> headers, RestCallback<T, E> callback) {
-        File file = getTestFile();
-        super.post(urlStr, file.toURI().getPath(), object, parameters, headers, callback);
-    }
-
-    private File getTestFile() {
-        File file;
-        try {
-            file = File.createTempFile("test_", ".jpg");
-            FileOutputStream out = new FileOutputStream(file);
-            InputStream in = getClass().getResourceAsStream("/com/sample/images/4stooges.jpg");
-            IOUtils.copy(in, out);
-            IOUtils.closeQuietly(in);
-            IOUtils.closeQuietly(out);
-        } catch (IOException e) {
-            throw new RuntimeException("error uploading file: " + e.getMessage(), e);
-        }
-        return file;
-    }
+    //    @Override
+    //    public <D, T, E> void post(String urlStr, String fileUri, D object, Map<String, Object> parameters,
+    //        Map<String, String> headers, RestCallback<T, E> callback) {
+    //        File file = getTestFile();
+    //        super.post(urlStr, file.toURI().getPath(), object, parameters, headers, callback);
+    //    }
+    //
+    //    private File getTestFile() {
+    //        File file;
+    //        try {
+    //            file = File.createTempFile("test_", ".jpg");
+    //            FileOutputStream out = new FileOutputStream(file);
+    //            InputStream in = getClass().getResourceAsStream("/com/sample/images/4stooges.jpg");
+    //            IOUtils.copy(in, out);
+    //            IOUtils.closeQuietly(in);
+    //            IOUtils.closeQuietly(out);
+    //        } catch (IOException e) {
+    //            throw new RuntimeException("error uploading file: " + e.getMessage(), e);
+    //        }
+    //        return file;
+    //    }
 
 
     @Override
     public <T> T getObject(String json, Class<T> type) {
         try {
-            return objectMapper.readValue(json, type);
+            return gson.fromJson(json, type);
         } catch (Exception e) {
             Log.e(TAG, "unable to parse json to type " + type, e);
             return null;
@@ -88,12 +78,7 @@ public class AndroidRestUtil extends BaseRestUtil {
             Log.w(TAG, "data to be posted is null, returning null String");
             return null;
         }
-        try {
-            return objectMapper.writeValueAsString(data);
-        } catch (IOException e) {
-            throw new RuntimeException(
-                "unable to create json from type " + data.getClass().getSimpleName(), e);
-        }
+        return gson.toJson(data);
     }
 
     @Override
